0ffb4a2b84086b2e5845278d485f78204703cf78
zerolxy <1056180778@qq.com>
2022-06-10 10:17:49 +0800

    'swiper轮播图动态实现'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,27 @@
-// index.js
-Page({})
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 轮播图数组
+    swiperList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 发送异步请求，获取后端数据
+    wx.request({
+      url: 'http://localhost:8080/product/findSwiper',
+      method:"GET",
+      success:(result)=>{
+        console.log(result)
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    })
+  }
+})
