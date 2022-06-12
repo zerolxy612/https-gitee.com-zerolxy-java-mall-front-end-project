5684d2e886a0d90d6e96597a0f3374b65236ccd4
zerolxy <1056180778@qq.com>
2022-06-12 17:10:57 +0800

    '获取后端商品分类接口实现'

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,20 +1,47 @@
-// pages/category/index.js
+// 导入request请求工具类
+import {
+  getBaseUrl,
+  requestUtil
+} from '../../utils/requestUtil.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    leftMenuList:[], // 左侧菜单数据
+    rightContext:[] // 右侧商品数据
 
   },
-
+// 所有商品类别数据
+  Cates:[],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getCates();
   },
 
+    // 获取商品分类数据
+    async getCates() {
+      const result = await requestUtil({
+        url: '/bigType/findCategories',
+        method: "GET"
+      });
+      this.Cates = result.message;
+      this.Cates.map((v)=>{
+        return v.name
+      })
+      let leftMenuList = this.Cates.map(v=>v.name)
+      let rightContext = this.Cates[0].smallTypeList;
+      this.setData({
+        leftMenuList,
+        rightContext
+      })
+
+
+    },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
